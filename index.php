3301351b27fffd35af355b333fcfff2d19a38db8
NarutoUchiha39 <anirudhata123@gmail.com>
2024-06-28 21:30:36 +0530

    Changed Home Page Layout

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-$title = "Keep In Touch" ;
-
 if(!isset($_SESSION["username"])){
 
         header("Location: /pages/login-page.php");
@@ -9,10 +7,20 @@ if(!isset($_SESSION["username"])){
         die;
 }
 
-$stylesheet='/css/index.css';
 include $_SERVER["DOCUMENT_ROOT"] ."/utils/messages.php";
-include 'includes/navbar.php'
+include $_SERVER["DOCUMENT_ROOT"] ."/utils/getURI.php"
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Keep In Touch</title>
+        <link rel="stylesheet" href="./css/index.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+ </head>
+ <body>
 
 <?php 
         
@@ -53,19 +61,59 @@ include 'includes/navbar.php'
    
         
         <div class="main_content" style="flex: 1;">
-                <div class="chats" style="margin-top:1rem">
-                        <?php
-                                foreach($people as $row){ ?>
-                                       <div  class="message"> 
-                                                <img style="margin-right:10px;height: 40px;width:40px;border-radius:1.1rem" src="<?php echo $row["profile_link"] ?>" alt="">
-                                                <?php echo $row["username"]; ?>
-                        
-                                        </div>
-                                <?php }
-                        ?>
-                </div>
 
-                <div class="display_chats" style="margin-left:1px solid gray">
+                <div class="chat_conatiner" style="display:flex;flex-direction:column;border-right:1px solid gray">
+                        <div class="user">
+                                <div class="details">
+                                        <img 
+                                        style="margin-right:10px;height: 40px;width:40px;border-radius:1.1rem" 
+                                        src="<?php echo $_SESSION["url"] ?>" alt="">
+
+                                        <?php echo $_SESSION["username"] ?>
+                                </div>
+
+                                <div class="logout">
+                                      <a href="<?php echo url('/pages/logout.php') ?>">
+                                        <i class="fas fa-sign-out-alt" style="color:white;font-size: larger;"></i>
+                                      </a>  
+                                </div>
+                                
+                        </div>
+
+                        <div class="chats" style="margin-top:1rem;flex:1">
+                                <?php
+                                        foreach($people as $row){ ?>
+                                        <div  class="message"> 
+
+                                                        <img style="margin-right:10px;
+                                                        height: 40px;width:40px;border-radius:1.1rem" 
+                                                        src="<?php echo $row["profile_link"] ?>" alt="">
+                                                
+                                                        <?php echo $row["username"]; ?>
+
+                                                </div>
+                                        <?php }
+                                ?>
+                        </div>
+
+                        <div class="footer">
+                                        
+                                        <i onclick="show_modal()" style="cursor:pointer;color:white" class="fa fa-plus" aria-hidden="true"></i>
+                                
+                                        <a href="<?php echo url("/pages/friend-request.php") ?>">
+                                                <i style="cursor:pointer;color:white" class="fa-solid fa-bell"></i>
+                                        </a>
+
+                                
+                        </div>
+                </div>
+                
+
+                <div class="display_chats" style="display:flex;flex-direction:column;margin-left:1px solid gray">
+
+                        <div class="title_bar">
+                                
+                        </div>
 
                 </div>
         </div>
@@ -86,9 +134,7 @@ include 'includes/navbar.php'
                 
         </form>
 
-        <div class="add_friends" onclick="show_modal(event)">
-                +
-        </div>
+        
         
     </div>
 
